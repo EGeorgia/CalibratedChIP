10bb78cdb4d616093403642ea1c30c210474b254
EGeorgia <emily.georgiades@linacre.ox.ac.uk>
2021-10-11 13:51:50 +0100

    updating downsampling python script

--- downSampling_calc.py
+++ downSampling_calc.py
@@ -1,50 +1,88 @@
+#!/usr/bin/env python
+
 #---------------------------------------------------------------------------
 # CALIBRATED ChIP-seq ANALYSIS: DownSampling Calculations
 # Emily Georgiades, March 2020.
+# Updated October 2021
 #---------------------------------------------------------------------------
 
-#!/usr/bin/env python
-# coding: utf-8
-
+# Import required modules:
 import pandas as pd
 import warnings
 import re
+import argparse
+
+# Option to specify whether input samples were included
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--inputs", help="Project contains input samples? (yes/no)")
+args = parser.parse_args()
+
 warnings.filterwarnings('ignore')
 
 # Load the textfile containing read counts:
-read_counts = pd.read_csv('./readCounts.txt', sep=' ')
+read_counts = pd.read_csv('/t1-data/project/fgenomics/egeorgia/Data/myData/ChIP-seq/2020-03-19_HEK293_RAPID-release_calibratedChIP/Analysis/readCounts.txt', sep=' ')
+
+# Load the textfile containing read counts:
 rep = []
+cond = []
 for i in read_counts['SAMPLE']:
     rep.append(re.sub('\.input$', '', i))
+    if "input" in i:
+        cond.append("input")
+    else:
+         cond.append("ChIP")
+    
 read_counts['REPLICATE'] = rep
+read_counts['SAMPLE'] = cond
 cols = read_counts.columns.tolist()
 cols = cols[-1:] + cols[:-1]
 read_counts = read_counts[cols]
-# Filter for the rows containing ChIP samples (rather than inputs controls)
-# Input samples must have "input" in sample name.
-sample_counts = read_counts[~read_counts['SAMPLE'].str.contains("input")]
-sample_counts['RATIO_MM10_UNIQ'] = sample_counts['GENOME_READS']/sample_counts['TOTAL_READS']
-sample_counts['RATIO_DM6_UNIQ'] = sample_counts['SPIKEIN_READS']/sample_counts['TOTAL_READS']
-sample_counts['RATIO_DM6vMM10'] = sample_counts['SPIKEIN_READS']/sample_counts['GENOME_READS']
-sample_counts['DM6_NORM'] = min(sample_counts['SPIKEIN_READS'])/sample_counts['SPIKEIN_READS'] # will need to change when have more data
 
-# Filter for the rows containing input controls
-input_counts = read_counts[read_counts['SAMPLE'].str.contains("input")]
-input_counts['RATIO_MM10_UNIQ'] = input_counts['GENOME_READS']/input_counts['TOTAL_READS']
-input_counts['RATIO_DM6_UNIQ'] = input_counts['SPIKEIN_READS']/input_counts['TOTAL_READS']
-input_counts['RATIO_DM6vMM10'] = input_counts['SPIKEIN_READS']/input_counts['GENOME_READS']
-input_counts['DM6_NORM'] = min(input_counts['SPIKEIN_READS'])/input_counts['SPIKEIN_READS'] # will need to change when have more data
+if args.inputs == 'yes':
+    print("Normalising by spike-in and input controls")
+    # Filter for the rows containing ChIP samples (rather than inputs controls)
+    # Input samples must have "input" in sample name.
+    sample_counts = read_counts[read_counts['SAMPLE'] != "input"]
 
-# Re-merge the sample and input rows
-merged = pd.merge(sample_counts, input_counts, on="REPLICATE")
-new_merged = merged.iloc[:,0:9]
-new_merged['INPUT_RATIO'] = merged['RATIO_DM6vMM10_y']
-new_merged.columns = ['REPLICATE','SAMPLE','TOTAL_READS','GENOME_READS','SPIKEIN_READS','RATIO_MM10_UNIQ','RATIO_DM6_UNIQ','RATIO_DM6vMM10','DM6_NORM','INPUT_RATIO']
+    # Calculate ratios:
+    sample_counts['RATIO_GENOME_UNIQ'] = (sample_counts['GENOME_READS'])/(sample_counts['TOTAL_READS'])
+    sample_counts['RATIO_SPIKEIN_UNIQ'] = sample_counts['SPIKEIN_READS']/sample_counts['TOTAL_READS']
+    sample_counts['RATIO_SPIKEINvGENOME'] = sample_counts['SPIKEIN_READS']/sample_counts['GENOME_READS']
+    sample_counts['SPIKEIN_NORM'] = min(sample_counts['SPIKEIN_READS'])/sample_counts['SPIKEIN_READS'] 
 
-# Calculate the downsampling ratios
-new_merged['DOWNSAMPLE_FRACTION'] = new_merged['INPUT_RATIO']*new_merged['DM6_NORM']
-new_merged['DOWNSAMPLE_FACTOR'] = new_merged['DOWNSAMPLE_FRACTION']/max(new_merged['DOWNSAMPLE_FRACTION'])*0.9999
-new_merged['READS_POST_DOWNSAMPLE'] = (new_merged['GENOME_READS']*new_merged['DOWNSAMPLE_FACTOR']).astype(int)
+    # Filter for the rows containing input controls
+    # Input samples must have "input" in sample name.
+    input_counts = read_counts[read_counts['SAMPLE'] == "input"]
 
-# Export results to textfile
-new_merged.to_csv('./downsamplingCalculations.txt', sep='\t', index=False, header=None)
+    # Calculate ratios:
+    input_counts['RATIO_GENOME_UNIQ'] = input_counts['GENOME_READS']/input_counts['TOTAL_READS']
+    input_counts['RATIO_SPIKEIN_UNIQ'] = input_counts['SPIKEIN_READS']/input_counts['TOTAL_READS']
+    input_counts['RATIO_SPIKEINvGENOME'] = input_counts['SPIKEIN_READS']/input_counts['GENOME_READS']
+    input_counts['SPIKEIN_NORM'] = min(input_counts['SPIKEIN_READS'])/input_counts['SPIKEIN_READS'] 
+
+    # Re-merge the sample and input rows
+    merged = pd.merge(sample_counts, input_counts, on="REPLICATE")
+    new_merged = merged.iloc[:,0:9]
+    new_merged['INPUT_RATIO'] = merged['RATIO_SPIKEINvGENOME_y']
+    new_merged.columns = ['REPLICATE','SAMPLE','TOTAL_READS','GENOME_READS','SPIKEIN_READS','RATIO_GENOME_UNIQ','RATIO_SPIKEIN_UNIQ','RATIO_SPIKEINvGENOME','SPIKEIN_NORM','INPUT_RATIO']
+    
+    # Calculate the downsampling ratios
+    new_merged['DOWNSAMPLE_FRACTION'] = new_merged['INPUT_RATIO']*new_merged['SPIKEIN_NORM']
+    new_merged['DOWNSAMPLE_FACTOR'] = new_merged['DOWNSAMPLE_FRACTION']/max(new_merged['DOWNSAMPLE_FRACTION'])*0.9999
+    new_merged['READS_POST_DOWNSAMPLE'] = (new_merged['GENOME_READS']*new_merged['DOWNSAMPLE_FACTOR']).astype(int)
+    
+    # Export results to textfile
+    new_merged.to_csv('./downsamplingCalculations.txt', sep='\t', index=False, header=None)
+
+else:
+    print("Proceeding with no input normalisation")
+    # Calculate ratios:
+    sample_counts['RATIO_GENOME_UNIQ'] = (sample_counts['GENOME_READS'])/(sample_counts['TOTAL_READS'])
+    sample_counts['RATIO_SPIKEIN_UNIQ'] = sample_counts['SPIKEIN_READS']/sample_counts['TOTAL_READS']
+    sample_counts['RATIO_SPIKEINvGENOME'] = sample_counts['SPIKEIN_READS']/sample_counts['GENOME_READS']
+    sample_counts['SPIKEIN_NORM'] = min(sample_counts['SPIKEIN_READS'])/sample_counts['SPIKEIN_READS'] 
+    sample_counts['DOWNSAMPLE_FACTOR'] = sample_counts['SPIKEIN_NORM']/max(new_merged['SPIKEIN_NORM'])*0.9999
+    sample_counts['READS_POST_DOWNSAMPLE'] = (sample_counts['GENOME_READS']*sample_counts['DOWNSAMPLE_FACTOR']).astype(int)
+    
+    # Export results to textfile
+    sample_counts.to_csv('./downsamplingCalculations.txt', sep='\t', index=False, header=None)
